b613c52f864105a0cb97d5351aa74361f8ac0bc0
INSEOP KIM <inseop0813@gmail.com>
2023-07-19 09:53:34 +0900

    project settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import routes from "./routes";
+
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+import Profile from "./screens/Profile";
+import Chatting from "./screens/Chatting";
+import { GlobalStyle, darkTheme, whiteTheme } from "./styles";
+
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+
 function App() {
-  return <div>Hi</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : whiteTheme}>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path={routes.home} element={isLogin ? <Home /> : <Login />} />
+          <Route path={routes.signup} element={<SignUp />} />
+          <Route path={routes.profile} element={<Profile />} />
+          <Route path={routes.chatting} element={<Chatting />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
